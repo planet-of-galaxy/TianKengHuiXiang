fcc2607c5556ba59fa02cf4e94fedc348e0f931b
planet <weiboxin100@gmail.comm>
2026-08-25 20:53:20 +0800

    Revert "完成RolePointController，可以自动生成角色，预留相机视角"
    This reverts commit 62fb3d44c2c55482c12ef9214453d65965571948.

--- Assets/Scripts/Controller/Player/RolePointController/RoleCreator.cs
+++ Assets/Scripts/Controller/Player/RolePointController/RoleCreator.cs
@@ -9,8 +9,6 @@ public class RoleCreator : MonoBehaviour, IController
 {
     private GameObject spawnedRole;
 
-    public bool IsIdle => spawnedRole == null;
-
     public void CreateRole(int runtimeIndex)
     {
         if (spawnedRole != null)
