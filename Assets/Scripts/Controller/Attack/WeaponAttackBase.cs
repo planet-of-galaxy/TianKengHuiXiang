63bffa64e4ad7e89de4a1eff99f8b0a83981635d
银河系的小星球 <weiboxin100@gmail.com>
2026-09-13 14:08:21 +0800

    refactor: 攻击监听改为直接依赖 IAttackA

--- Assets/Scripts/Controller/Attack/WeaponAttackBase.cs
+++ Assets/Scripts/Controller/Attack/WeaponAttackBase.cs
@@ -1,33 +1,11 @@
-using QFramework;
 using UnityEngine;
 
 /// <summary>
 /// 武器攻击基类：挂在武器物体上。
-/// 监听 <see cref="PlayerAttackEvent"/>（由 AttackListener 在玩家左键单击时发出），
-/// 事件发生时执行一次 <see cref="AttackA"/>。
+/// 实现 IAttackA，由同一物体上的 AttackListener 调用 AttackA。
 /// </summary>
-public abstract class WeaponAttackBase : MonoBehaviour, IAttackA, IController
+public abstract class WeaponAttackBase : MonoBehaviour, IAttackA
 {
-    private IUnRegister _playerAttackUnRegister;
-
-    public IArchitecture GetArchitecture() => TianArchitecture.Interface;
-
-    private void OnEnable()
-    {
-        _playerAttackUnRegister = this.RegisterEvent<PlayerAttackEvent>(OnPlayerAttackEvent);
-    }
-
-    private void OnDisable()
-    {
-        _playerAttackUnRegister?.UnRegister();
-        _playerAttackUnRegister = null;
-    }
-
-    private void OnPlayerAttackEvent(PlayerAttackEvent e)
-    {
-        AttackA();
-    }
-
     /// <summary>
     /// 执行一次 A 型攻击，返回本次造成的伤害信息。
     /// 具体命中判定与伤害结算由子类实现；命中后可调用 <see cref="TryDeliverDamage"/> 把伤害交给目标。
